a3b8631e90eda9c2a73e8ad919e4f7bae1bbc598
Aurielle Chell <71284853+auriellechell@users.noreply.github.com>
2020-11-23 19:53:22 -0500

    Update script.js
    added new java

--- script.js
+++ script.js
@@ -1,51 +1,20 @@
+let dateElement=document.querySelector("#date");
+let currentTime= new Date();
+let hours= currentTime.getHours();
+let minutes=currentTime.getMinutes();
+console.log(hours,minutes);
 
-
-function formatDate(date) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-  ];
-
-  let currentYear = date.getFullYear();
-  let currentDay = days[date.getDay()];
-  let currentMonth = months[date.getMonth()];
-  let currentDate = date.getDate();
-
-  let formattedDate = `${currentDay}, ${currentMonth} ${currentDate}, ${currentYear}`;
-
-  return formattedDate;
-}
-
-
-
-
-//days of week array//
+let dayIndex=currentTime.getDay();
 let days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
 let day=days[date.getDay()];
+
+
+dateElement.innerHTML= `${hours}:${minutes};
+
+
 //api info//
 let apiKey = "a67bcee8bc3862ba2da27468ca9d60a2";
 let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
 
 let latitude= position.coords.latitude;
 let longitude= position.coords.longitude;
