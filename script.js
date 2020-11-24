ed7e2d5a6db70dd1f983fec167a85969ddc2dad6
Aurielle Chell <71284853+auriellechell@users.noreply.github.com>
2020-11-23 23:31:57 -0500

    Update script.js
    adding java

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function showTempature(response) {
+    let tempature= Math.round(response.data.main.temp);
+    let city= response.data.name;
+
+}
+
+ let apiKey = "a67bcee8bc3862ba2da27468ca9d60a2";
+ let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showTempature);
+
+
 function search(event){
     event.preventDefault();
       
